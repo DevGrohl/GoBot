5a004c0a55902380f0eb3d062dcd8c3034beb7b2
Miguel Lares <c.code.jp@gmail.com>
2024-06-19 00:25:31 -0600

    Basic bot module

---
+++ internal/discord/bot.go
@@ -0,0 +1,48 @@
+package bot
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var BotToken string
+
+func checkNilErr(e error) {
+	if e != nil {
+		log.Fatal("Error message")
+	}
+}
+
+func Run() {
+	// create a session
+	discord, err := discordgo.New("Bot " + BotToken)
+	checkNilErr(err)
+
+	// add a event handler
+	discord.AddHandler(newMessage)
+
+	// open Session
+	discord.Open()
+	defer discord.Close() // close session after func termination
+
+	fmt.Println("Bot running...")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+}
+
+func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
+	if message.Author.ID == discord.State.User.ID {
+		return
+	}
+
+	switch {
+	case strings.Contains(message.Content, "!help"):
+		discord.ChannelMessageSend(message.ChannelID, "Hello World!")
+	}
+}
